d3c39253c2b9644df3a0c92d14c997ba880a4a5c
jke <constantinekulak@proton.me>
2023-07-08 15:14:18 +0300

    Initial Commit

---
+++ Gallery/GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalleryApp.swift
+//  Gallery
+//
+//  Created by Constantine Kulak on 08.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
